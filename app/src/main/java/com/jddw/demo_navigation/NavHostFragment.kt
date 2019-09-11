c50b9e993a04feba51cb9985addb79755d4b1c54
lujianye <lujianye1003@gmail.com>
2019-09-11 15:19:28 +0800

    11111111

---
+++ app/src/main/java/com/jddw/demo_navigation/NavHostFragment.kt
@@ -0,0 +1,14 @@
+package com.jddw.demo_navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NavHostFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_navhost, container, false)
+    }
+}
